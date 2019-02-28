1077e74b867e5f432363dcf7804d1b1d3228b032
Gijsjan <agijsbro@gmail.com>
2019-02-28 15:54:54 +0100

    Fix re-index only on change

--- build/db/update-project.js
+++ build/db/update-project.js
@@ -2,18 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("./utils");
 const es_1 = require("../es");
+function has(props, key) {
+    return props.hasOwnProperty(key) && props[key] != null;
+}
 async function updateProject(slug, props) {
     const setters = Object.keys(props).map((prop, index) => `${prop} = $${index + 2}`).concat('updated = NOW()');
     const sql = `UPDATE project
 				SET ${setters.join(',')}
 				WHERE slug = $1
 				RETURNING *`;
-    if (props.hasOwnProperty('metadata_extractor') && props.metadata_extractor != null ||
-        props.hasOwnProperty('extractors') && props.extractors != null) {
+    const hasMetadataExtractor = has(props, 'metadata_extractor');
+    const hasExtractors = has(props, 'extractors');
+    const hasSplitter = has(props, 'splitter');
+    const hasFacsimileExtractor = has(props, 'facsimile_extractor');
+    if (hasMetadataExtractor || hasExtractors || hasSplitter || hasFacsimileExtractor) {
         const prevProject = await utils_1.selectOne('project', 'slug', slug);
-        if (prevProject.metadata_extractor !== props.metadata_extractor ||
-            prevProject.extractors !== props.extractors) {
-            es_1.default(slug, props.metadata_extractor, props.extractors);
+        if ((hasMetadataExtractor && prevProject.metadata_extractor !== props.metadata_extractor) ||
+            (hasExtractors && JSON.stringify(prevProject.extractors) !== JSON.stringify(props.extractors)) ||
+            (hasSplitter && prevProject.splitter !== props.splitter) ||
+            (hasFacsimileExtractor && prevProject.facsimile_extractor !== props.facsimile_extractor)) {
+            es_1.default(slug, props.splitter, props.metadata_extractor, props.facsimile_extractor, props.extractors);
         }
     }
     const result = await utils_1.execSql(sql, [slug].concat(Object.keys(props).map(prop => {
--- src/db/update-project.ts
+++ src/db/update-project.ts
@@ -3,6 +3,10 @@ import { Project } from '../models'
 import updateIndex from '../es'
 // import syncMetadata from './sync-metadata'
 
+function has(props: Partial<Project>, key: keyof Project) {
+	return props.hasOwnProperty(key) && props[key] != null
+}
+
 async function updateProject(slug: string, props: Partial<Project>): Promise<Project> {
 	const setters = Object.keys(props).map((prop, index) => `${prop} = $${index + 2}`).concat('updated = NOW()')
 	const sql = `UPDATE project
@@ -10,24 +14,24 @@ async function updateProject(slug: string, props: Partial<Project>): Promise<Pro
 				WHERE slug = $1
 				RETURNING *`
 
-	// Only update the index if the updated props contain metadata_extractor or extractors
-	if (
-		props.hasOwnProperty('metadata_extractor') && props.metadata_extractor != null ||
-		props.hasOwnProperty('extractors') && props.extractors != null
-	) {
+	const hasMetadataExtractor = has(props, 'metadata_extractor')
+	const hasExtractors = has(props, 'extractors')
+	const hasSplitter = has(props, 'splitter')
+	const hasFacsimileExtractor = has(props, 'facsimile_extractor')
+
+	if (hasMetadataExtractor || hasExtractors || hasSplitter || hasFacsimileExtractor) {
 		// Get the previous project from the DB 
 		const prevProject = await selectOne('project', 'slug', slug) as Project
 
 		// Only update the index if metadata_extractor or extractors has changed
 		if (
-			prevProject.metadata_extractor !== props.metadata_extractor ||
-			prevProject.extractors !== props.extractors
+			(hasMetadataExtractor && prevProject.metadata_extractor !== props.metadata_extractor) ||
+			(hasExtractors && JSON.stringify(prevProject.extractors) !== JSON.stringify(props.extractors)) ||
+			(hasSplitter && prevProject.splitter !== props.splitter) ||
+			(hasFacsimileExtractor && prevProject.facsimile_extractor !== props.facsimile_extractor)
 		) {
 			// Update index is async, but we don't have to wait for it
-			updateIndex(slug, props.metadata_extractor, props.extractors)
-			// .then(metadataKeys => {
-			// 	syncMetadata(prevProject.id, metadataKeys)
-			// })
+			updateIndex(slug, props.splitter, props.metadata_extractor, props.facsimile_extractor, props.extractors)
 		}
 	}
 
